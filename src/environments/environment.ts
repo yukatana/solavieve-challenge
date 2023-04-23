7cdab6b80f34446c095e820fe9a2ade036b6391b
yukatana <rommel.aranguren@gmail.com>
2023-04-23 13:36:30 -0300

    added firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'solavieve-challenge',
+    appId: '1:498768889993:web:8416e25a0181ee12235e58',
+    storageBucket: 'solavieve-challenge.appspot.com',
+    apiKey: 'AIzaSyDpdLcvSTgEXpXUl2g18GGjP7Fx4KeRwXY',
+    authDomain: 'solavieve-challenge.firebaseapp.com',
+    messagingSenderId: '498768889993',
+  },
   production: false
 };
 
